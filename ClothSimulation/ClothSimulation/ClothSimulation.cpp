b71b19a20cd18f44a69595afb5652c2aad962b56
Merloran <poncyleuszmaciej@gmail.com>
2023-10-11 14:48:57 +0200

    Initial commit

---
+++ ClothSimulation/ClothSimulation/ClothSimulation.cpp
@@ -0,0 +1,12 @@
+﻿#include <spdlog/spdlog.h>
+#include <imgui.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <tiny_gltf.h>
+#include <glm/glm.hpp>
+#include <stb_image.h>
+
+int main()
+{
+	SPDLOG_INFO("Hello World!");
+}
